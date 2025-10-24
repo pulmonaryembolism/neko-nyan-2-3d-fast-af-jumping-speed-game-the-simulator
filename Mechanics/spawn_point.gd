ca74bb96d5398103f8600ef4bf8c034285f79fc4
pulmonaryembolism <notactuallytherealpyon@gmail.com>
2025-10-24 13:07:51 -0700

    broken shit :(

--- Mechanics/spawn_point.gd
+++ Mechanics/spawn_point.gd
@@ -2,4 +2,4 @@ extends Node3D
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("reset"):
-		self.global_position = $spawn_point.global_position
+		get_node("CharacterBody").global_position = $spawn_point.global_position
