b90624f6a93302ce18c165e92835f6518f07c035
Adan O'Toole <72539191+AdanOToole@users.noreply.github.com>
2025-10-29 16:42:30 -0700

    UI fuck yeah baby

--- title_screen.gd
+++ title_screen.gd
@@ -1,13 +1,20 @@
 extends Control
 
+func _ready():
+	$StartButton.pressed.connect(_on_StartButton_pressed)
+	$OptionsButton.pressed.connect(_on_OptionsButton_pressed)
+	$QuitButton.pressed.connect(_on_QuitButton_pressed)
 
-
-
-func _on_quit_button_pressed() -> void:
+func _on_QuitButton_pressed():
 	get_tree().quit()
-	pass # Replace with function body.
 
-
-func _on_start_button_pressed() -> void:
+func _on_StartButton_pressed():
 	get_tree().change_scene_to_file("res://Scenes/test.tscn")
-	pass # Replace with function body.
+
+func _on_OptionsButton_pressed():
+	var options = preload("res://Scenes/Options.tscn").instantiate()
+	options.set_source(options.Source.TITLE)
+	get_tree().root.add_child(options)
+	$StartButton.hide()
+	$OptionsButton.hide()
+	$QuitButton.hide()
