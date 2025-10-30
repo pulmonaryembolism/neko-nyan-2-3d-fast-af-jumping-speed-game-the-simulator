dcfde158efb8e3ec2d03bbd3fad07c0a91caaaf7
pulmonaryembolism <notactuallytherealpyon@gmail.com>
2025-10-30 16:16:54 -0700

    fov movement and timer

--- Mechanics/timer.gd
+++ Mechanics/timer.gd
@@ -10,6 +10,9 @@ signal time_changed(minutes, seconds, msec)
 func _process(delta: float) -> void:
 	time += delta
 	msec = fmod(time, 1) * 100
-	seconds = fmod(time, 3600) / 60
-	minutes = fmod(time, 60)
+	seconds = fmod(time, 60)
+	minutes = fmod(time, 3600) / 60
 	time_changed.emit(minutes, seconds, msec)
+
+func stop() -> void:
+	set_process(false)
