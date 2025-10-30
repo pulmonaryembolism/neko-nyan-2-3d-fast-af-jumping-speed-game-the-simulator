e76fbfafb812b4f671aaa8dac47f4b976f2f4c24
Adan O'Toole <72539191+AdanOToole@users.noreply.github.com>
2025-10-30 12:51:27 -0700

    end screen framework

--- title_screen.gd
+++ title_screen.gd
@@ -1,9 +1,13 @@
 extends Control
 
+@onready var start_button = $MarginContainer/VBoxContainer/StartButton
+@onready var options_button = $MarginContainer/VBoxContainer/OptionsButton
+@onready var quit_button = $MarginContainer/VBoxContainer/QuitButton
+
 func _ready():
-	$StartButton.pressed.connect(_on_StartButton_pressed)
-	$OptionsButton.pressed.connect(_on_OptionsButton_pressed)
-	$QuitButton.pressed.connect(_on_QuitButton_pressed)
+	start_button.pressed.connect(_on_StartButton_pressed)
+	options_button.pressed.connect(_on_OptionsButton_pressed)
+	quit_button.pressed.connect(_on_QuitButton_pressed)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 
@@ -17,6 +21,6 @@ func _on_OptionsButton_pressed():
 	var options = preload("res://Scenes/Options.tscn").instantiate()
 	options.set_source(options.Source.TITLE)
 	get_tree().root.add_child(options)
-	$StartButton.hide()
-	$OptionsButton.hide()
-	$QuitButton.hide()
+	start_button.hide()
+	options_button.hide()
+	quit_button.hide()
