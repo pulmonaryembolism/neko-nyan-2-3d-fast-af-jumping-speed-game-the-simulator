13d8a4c827f44efe06d8498f7666e3060401216b
ArchimedesD <addickso@ucsc.edu>
2025-11-02 16:38:15 -0800

    Revert "Merge branch 'main' of https://github.com/pulmonaryembolism/neko-nyan-2-3d-fast-af-jumping-speed-game-the-simulator"
    This reverts commit 04fb05f16fb8cd18c44bd5869fa013ce4f4e86ca, reversing
    changes made to 8325b422c0bb388f724d6593edf19cdf8c202ed8.

--- Assets/Global.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var cm_per_360 := 22.0
-var dpi := 800.0
-var final_time := ""
-var stop_timer := false
-var reset_timer := false
--- Assets/Mechanics/spawn_point.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-func _physics_process(delta):
-	if Input.is_action_just_pressed("reset"):
-		get_node("CharacterBody3D").global_position = $spawn_point.global_position
-		Global.reset_timer = true
