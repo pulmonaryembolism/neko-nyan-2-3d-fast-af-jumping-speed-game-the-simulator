7aabbf6bc1fc51860ab7ceb0eeb19272493a3a07
ArchimedesD <addickso@ucsc.edu>
2025-11-05 17:30:10 -0800

    Revert "Merge branch 'main' of https://github.com/pulmonaryembolism/neko-nyan-2-3d-fast-af-jumping-speed-game-the-simulator"
    This reverts commit e24d617b20139c40296bd0359197ccc478b1ee82, reversing
    changes made to 8c6265fa4f15aac6b66e4aadc579ae3a6ff7212d.

--- Mechanics/checkpoint.gd
+++ Mechanics/checkpoint.gd
@@ -1,22 +1,8 @@
 extends Area3D
 
-@export var checkpoint_id: int = 0
-var activated: bool = false
+@onready var level = get_tree().get_first_node_in_group("spawn_point")
 
-signal checkpoint_activated(checkpoint_id: int)
-
-func _ready():
-		connect("body_entered", _on_body_entered)
-		CheckpointManager.register_checkpoint()
-		connect("checkpoint_activated", Callable(CheckpointManager, "on_checkpoint_activated"))
-
-		
-func _on_body_entered(body):
-	if activated: 
-		return
-		
-	if body.is_in_group("Player"):
-		activated = true
-		emit_signal("checkpoint_activated", checkpoint_id)
-
-	
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("CharacterBody3D"):
+		level.set_active_checkpoint(self)
+		print("setting active checkpoint...")
