2ed9fd847358143881bec2b136835227eb43c955
pulmonaryembolism <notactuallytherealpyon@gmail.com>
2025-11-06 19:36:36 -0800

    prep save screnn

--- end_screen.gd
+++ end_screen.gd
@@ -5,12 +5,12 @@ extends Control
 
 func _ready():
 	$MarginContainer/VBoxContainer/Time.text = Global.final_time
-	replay_button.pressed.connect(_on_StartButton_pressed)
+	replay_button.pressed.connect(_on_ReplayButton_pressed)
 	quit_button.pressed.connect(_on_QuitButton_pressed)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
 
-func _on_StartButton_pressed():
+func _on_ReplayButton_pressed():
 	get_tree().change_scene_to_file("res://Scenes/test.tscn")
