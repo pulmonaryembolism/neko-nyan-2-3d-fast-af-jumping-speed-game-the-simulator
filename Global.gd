566865663852597093ef97cf5731739578fea063
pulmonaryembolism <notactuallytherealpyon@gmail.com>
2025-11-06 18:55:43 -0800

    make end screen not atrocious

--- Global.gd
+++ Global.gd
@@ -2,6 +2,6 @@ extends Node
 
 var cm_per_360 := 22.0
 var dpi := 800.0
-var final_time := ""
+var final_time := "Time: 00:00:000"
 var stop_timer := false
 var reset_timer := false
--- end_screen.gd
+++ end_screen.gd
@@ -1,13 +1,12 @@
 extends Control
 
-@onready var start_button = $MarginContainer/VBoxContainer/StartButton
+@onready var start_button = $MarginContainer/VBoxContainer/ReplayButton
 @onready var options_button = $MarginContainer/VBoxContainer/OptionsButton
 @onready var quit_button = $MarginContainer/VBoxContainer/QuitButton
 
 func _ready():
 	$MarginContainer/VBoxContainer/Time.text = Global.final_time
 	start_button.pressed.connect(_on_StartButton_pressed)
-	options_button.pressed.connect(_on_OptionsButton_pressed)
 	quit_button.pressed.connect(_on_QuitButton_pressed)
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
@@ -16,11 +15,3 @@ func _on_QuitButton_pressed():
 
 func _on_StartButton_pressed():
 	get_tree().change_scene_to_file("res://Scenes/test.tscn")
-
-func _on_OptionsButton_pressed():
-	var options = preload("res://Scenes/Options.tscn").instantiate()
-	options.set_source(options.Source.TITLE)
-	get_tree().root.add_child(options)
-	start_button.hide()
-	options_button.hide()
-	quit_button.hide()
